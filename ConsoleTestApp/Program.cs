a16f137eeb9232baaed99ca6e73444e956365575
PrathamAditya <prathamadityasalhotra@gmail.com>
2026-04-15 20:24:35 +0530

    Commit: Core files created in Abstration and Core project and ConsoleTestApp project added for testing of code.

---
+++ ConsoleTestApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExecutionFabric.Abstractions;
+using ExecutionFabric.Core;
+
+namespace ConsoleTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _ = new DefaultExecutor();
+        }
+    }
+}
---
+++ src/ExecutionFabric.Abstractions/ExecutionResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExecutionFabric.Abstractions
+{
+    public class ExecutionResult
+    {
+    }
+}
